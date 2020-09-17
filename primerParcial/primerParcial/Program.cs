545b536702be712f3138dff131fc85ec77d1a094
florencialujan <florlujaan@gmail.com>
2020-09-16 21:55:12 -0300

    Primer commit

---
+++ primerParcial/primerParcial/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primerParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opc = 0;
+            int cantidadDeCamisas = 0;
+            int desc;
+            string entrada;
+            float preciofinal=0;
+            int opc2 = 0;
+            do {
+                
+                Console.WriteLine("        Camisas PRADBIT – Ventas minoristas y mayoristas");
+                Console.WriteLine("----------------------------------------------------------------------------");
+                Console.WriteLine("MENU PRINCIPAL");
+                Console.WriteLine("1. Añadir camisa al carro de compras");
+                Console.WriteLine("2. Eliminar camisa del carro de compras");
+                Console.WriteLine("3. SALIR");
+                entrada = Console.ReadLine();
+                opc = Convert.ToInt32(entrada);
+
+                switch (opc)
+                {
+                    case 1:
+                        cantidadDeCamisas = cantidadDeCamisas + 1; break;
+                    case 2:
+                        cantidadDeCamisas = cantidadDeCamisas - 1; break;
+                    case 3:
+                        Console.Clear();
+                        do
+                        {
+                            Console.WriteLine("¿Estas seguro/a de que deseas salir del programa?");
+                            Console.WriteLine("1. SI");
+                            Console.WriteLine("2. NO");
+                            entrada = Console.ReadLine();
+                            opc2 = Convert.ToInt32(entrada);
+                        } while (opc2 != 1 && opc2 != 2);
+                        break;
+                }
+
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine("Cantidad de camisas en el carro: " + cantidadDeCamisas);
+                desc = descuentoAplicado(ref cantidadDeCamisas);
+                Console.WriteLine("Precio unitario: $1000");
+                Console.WriteLine("Precio total sin descuento: " + cantidadDeCamisas * 1000);
+                Console.WriteLine("Tipo de descuento aplicado: " + desc + " % del total de la compra");
+                preciofinal = precioFinal(ref cantidadDeCamisas, ref desc);
+                Console.WriteLine("PRECIO FINAL CON DESCUENTO APLICADO: " + preciofinal);
+
+            } while (opc2 != 1);
+
+
+        }
+
+        static int descuentoAplicado(ref int cantidadDeCamisas)
+        {
+            int porcentajedesc=0;
+
+            if (cantidadDeCamisas < 3)
+            {
+                porcentajedesc = 0;
+            }
+
+            if (cantidadDeCamisas >= 3 && cantidadDeCamisas <= 5)
+            {
+                porcentajedesc = 10;
+            }
+
+            if (cantidadDeCamisas > 5)
+            {
+                porcentajedesc = 20;
+            }
+            return porcentajedesc;
+        }
+
+        static float precioFinal(ref int cantidadDeCamisas, ref int desc)
+        {
+            float preciofinal;
+            float descuento;
+
+            descuento = cantidadDeCamisas * 1000 * desc / 100;
+            preciofinal = cantidadDeCamisas * 1000 - descuento;
+            return preciofinal;
+        }
+    }
+
+}
+
+
---
+++ readme.md.txt
@@ -0,0 +1,15 @@
+PARCIAL 1 - LABORATORIO 2 - ALUMNA: LUJAN, MARIA FLORENCIA.
+
+
+1)¿Qué diferencia hay entre un error de sintaxis y un error de lógica (falla)?
+Los errores de sintaxis son detectados por el compilador antes de ejecutar el programa. Estos podrían ser faltas de ; al fin de la sentencia, palabras claves mal escritas etc.
+Los errores lógicos son más difíciles de localizar, estos se deben a una mala implementación de la solución de un problema, que no nos arrojaría resultados o los resultados serían incorrectos.
+
+2)¿Cuáles son los operadores de una expresión lógica?
+Son: Y(conjunción), O(disyuncion) y No(negacion). En ingles: and, or y not. 
+
+3)La refactorización de código: ¿Agrega/quita funcionalidad al programa?
+No, no agrega ni quita funcionalidades. Sino que las optimiza, haciendo que el codigo sea mas eficiente, por ejemplo reduciendo la cantidad de lineas, o por ejemplo sea más facil de leer para otros programadores.
+
+4)Dentro de un ArrayList: ¿se pueden insertar valores de distinto tipo?
+SÍ. Es una de las caracteristicas de los Arraylist.
